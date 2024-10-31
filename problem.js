969a80cc53cd3335c0af4ec264d6d17f28b04ee9
Jamille <154861506+Strawb3rryjam@users.noreply.github.com>
2024-10-31 14:30:35 -0700

    Update problem.js
    For midterm! 👯‍♀️

--- problem.js
+++ problem.js
@@ -13,3 +13,52 @@ CHALLENGE
 - If the password is less than 5 characters, log out "Your password is too short!".
 */
 
+/*JAMILLE'S CODE!*/
+
+//function
+
+function passwordCheck(userInput, userPass) {
+  // checks for length
+  if (userPass.length < 5) {
+    console.log("Your password is too short!");
+    return;
+  }
+  //checks input forgot
+  else if (userInput === "forgot") {
+    console.log("Here is a hint");
+    console.log(
+      "This password you set should not be used because it glitches the system"
+    );
+
+    //checks input reset
+  } else if (userInput === "reset") {
+    console.log("Let's reset your account");
+    console.log(
+      "This password you set should not be used because it glitches the system"
+    );
+
+    //checks for normal granted access
+  } else if (userInput === userPass) {
+    console.log("Access Granted!");
+
+    //checks for no granted access
+  } else {
+    console.log("Access Denied!");
+  }
+}
+
+/*FUNCTION CALLING TIME!*/
+//problem 1
+passwordCheck("MLPlover123", "MLPlover123"); // Access Granted!
+
+//problem 2
+passwordCheck("MLPhater123", "MLPlover123"); // Access Denied!
+
+//problem 3
+passwordCheck("forgot", "MLPlover123"); // Here is a hint + The password you set should not be used because it glitches the system
+
+//problem 4
+passwordCheck("reset", "MLPlover123"); // Lets reset your account + "This password you set should not be used because it glitches the system
+
+//problem 5
+passwordCheck("smol", "smol"); // Your password is too short
